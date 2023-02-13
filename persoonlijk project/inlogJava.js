07f549c80b6581ab7d8e61412bdd5e10be69e6f9
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-02-13 09:52:39 +0100

    commit

--- persoonlijk project/inlogJava.js
+++ persoonlijk project/inlogJava.js
@@ -1,7 +1,7 @@
 let naam_ingelogd = " "
 
 function handlelogin(){
-    let naam = document.getElementById("Inputnaam").value; 
+    let Naam = document.getElementById("Inputnaam").value; 
     console.log("u heeft of de knop gedrukt")
     if (naam_ingelogd == ""){
         console.log("u bent ingelogd"); 
