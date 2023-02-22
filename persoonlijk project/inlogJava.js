7f4904eccafc76b273b962fff926accfa98b231b
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-02-22 09:33:29 +0100

    commit

--- persoonlijk project/inlogJava.js
+++ persoonlijk project/inlogJava.js
@@ -1,13 +1,29 @@
-let naam_ingelogd = " "
-
-let naam_ingelog = "";
+let LoggedIn = ["tobias"];
 
 function handleLogin(){
     let naam = document.getElementById("inputNaam").value;
-    console.log("U hebt op de knop geklikt!");
-    if (naam_ingelog === ""){
-        naam_ingelog = naam;
-        console.log("U bent ingelogd: " + naam);
-        document.getElementById("inputNaam").value = "";
+    if (naam.length <= 0){
+        alert("voer een naam in!")
+        return
     }
+    console.log("U hebt op de knop geklikt!");
+    if (LoggedIn.includes(naam)){
+        LoggedIn.splice(LoggedIn.indexOf(naam),  1)
+        console.log(" U bent uitgelogd" + naam);
+        document.getElementById("melding").innerText = "U bent uitgelogd " + naam;
+    } else {
+        LoggedIn.push(naam);
+        console.log(LoggedIn);
+        document.getElementById("melding").innerText = "U bent ingelogd " + naam;
+    }
+    document.getElementById("inputNaam").value = "";
+    console.log(LoggedIn)
+    
+
+    // console.log("U bent ingelogd: " + naam);
+    // document.getElementById("inputNaam").value = "";
+    // document.getElementById("melding").innerText = "gelukt"; 
+
 }
+
+document.getElementById("inlogbutton").onclick = handleLogin;
