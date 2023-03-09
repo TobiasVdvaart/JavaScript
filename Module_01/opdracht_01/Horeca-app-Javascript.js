2ca0d790c31a7b4f61907519b747583efebd30e0
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-03-09 09:11:30 +0100

    commit

--- Module_01/opdracht_01/Horeca-app-Javascript.js
+++ Module_01/opdracht_01/Horeca-app-Javascript.js
@@ -5,3 +5,9 @@ var Dranklijst = {
 };
 
 console.log(Dranklijst)
+
+let vraag = prompt("wat wilt u drinken? Wijn: 3,99 Bier: 3,99 Cola: 1,99")
+
+if(vraag != Dranklijst) {
+    console.log("het drankje wat u wilt hebben we niet!")
+}
