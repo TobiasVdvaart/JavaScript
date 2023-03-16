0df12a97f48831bbe1ea384f05db513bdd07472d
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-03-16 09:18:32 +0100

    commit

--- Module_01/opdracht_01/Horeca-app-Javascript.js
+++ Module_01/opdracht_01/Horeca-app-Javascript.js
@@ -5,18 +5,42 @@ const Drankenlijst =
     'Wijn': 3.99
 };
 
-let rekening = new Object();
-rekening["Bier"] = aantal_drankjes_02;
-rekening["wijn"] = aantal_drankjes_02;
-console.log(rekening)
+let Kladblok = new Object();
 
-let vraag_01 = parseInt(prompt("Voer 1 in"));
-let aantal_drankjes = parseInt(prompt("Hoeveel drankjes wilt u?,"));
 
-let aantal_drankjes_02 = ''
-for (let i =vraag_01; i <= aantal_drankjes; i++) {
-         aantal_drankjes_02 = prompt("Welke drankjes wilt u?, Fris: 2,99 Wijn: 3,99 Bier: 3,99");
-  }
+while (true){
+     let drank = prompt("welke drankjes wilt u?")
+    let aantal_drankjes = prompt("hoeveel wilt u er?")
+
+    Kladblok[drank] = aantal_drankjes;
+    console.log(Kladblok)
+if (drank == "no"){
+    break
+}
+
+}
+
+
+
+// stap 1 is voeg een loop toe
+//stap 2 toevoegenen en updaten
+//laaste stap bon maken
+
+
+
+
+
+// rekening["Bier"] = 3;
+// rekening["wijn"] = 3;
+// console.log(rekening)
+
+// let vraag_01 = parseInt(prompt("Voer 1 in"));
+// let aantal_drankjes = parseInt(prompt("Hoeveel drankjes wilt u?,"));
+
+// let aantal_drankjes_02 = ''
+// for (let i =vraag_01; i <= aantal_drankjes; i++) {
+//          aantal_drankjes_02 = prompt("Welke drankjes wilt u?, Fris: 2,99 Wijn: 3,99 Bier: 3,99");
+//   }
 
 
 
--- persoonlijk project/inlogJava.js
+++ persoonlijk project/inlogJava.js
@@ -1,33 +1,55 @@
-let LoggedIn = [];
-function getElementByName(LoggedIn, naam){
+let loggedIn = [];
 
-    return -1;
+function getGewerktString(datum1, datum2){
+    const seconden = (datum1 - datum2) /1000;
+    let minuten = 0;
+    if(seconden > 60){
+        minuten = Math.floor(seconden/60);
+    }
+    seconden -= verschil * 60;
+    return  'gewerkt ' + minuten + 'minuten' + seconden + "seconden";
+}
+
+
+
+function getIndexOfElementByName(lijst, naam){
+    for(let x = 0; x < lijst.length; x++){
+        if(lijst[x]['naam'] === naam){
+            return x;
+        }
+    }
+    return null;
 }
 
 function handleLogin(){
-    console.log(LoggedIn);
+    console.log(loggedIn);
     console.log(Date())
 
     let naam = document.getElementById("inputNaam").value;
     if (naam.length <= 0){
-        alert("voer een naam in!")
-        return
+        alert("Voer een naam in!")
+        return;
     }
-    console.log("U hebt op de knop geklikt!");
-    if (LoggedIn.includes(naam)){
-        LoggedIn.splice(LoggedIn.indexOf(naam),  1)
-        console.log(" U bent uitgelogd" + naam);
-        document.getElementById("melding").innerText = "U bent uitgelogd " + naam;
-    } else {
-        LoggedIn.push({"de naam" : naam, "de datum": Date()});
-        console.log(LoggedIn);
-        document.getElementById("melding").innerText = "U bent ingelogd " + naam;
-    }
-    document.getElementById("inputNaam").value = "";
-    console.log(LoggedIn)
-    
+    console.log("U hebt op de knop geklikt.");
 
+    const positie = getIndexOfElementByName(loggedIn, naam);
+    if (positie !== null){
+        const oldDate = loggedIn[positie]['moment']
+        const newDate = new Date();
+        const verschilDate = (newDate - oldDate) /1000;
+        console.log(verschilDate)
+        loggedIn.splice(positie, 1)
+        console.log("U bent uitgelogd: " + naam);
+        document.getElementById('melding').innerText = "U bent uitgelogd: " + naam + getGewerktString(newDate, oldDate);
+
+    } else{
+        loggedIn.push({'naam': naam, 'tijd': new Date()});
+        console.log("U bent ingelogd: " + naam);
+        document.getElementById('melding').innerText = "U bent ingelogd: " + naam;
+    }
+
+    document.getElementById("inputNaam").value = "";
+    console.log(loggedIn);
 }
 
-document.getElementById("inlogbutton").onclick = handleLogin;  
-
+document.getElementById("button").onclick = handleLogin;
