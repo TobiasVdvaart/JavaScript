9ae45eab29ba989d9343ec25583a1fc3ef01243e
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-03-16 10:18:14 +0100

    commit

--- Module_01/opdracht_01/Horeca-app-Javascript.js
+++ Module_01/opdracht_01/Horeca-app-Javascript.js
@@ -9,13 +9,16 @@ let Kladblok = new Object();
 
 
 while (true){
-     let drank = prompt("welke drankjes wilt u?")
-    let aantal_drankjes = prompt("hoeveel wilt u er?")
-
-    Kladblok[drank] = aantal_drankjes;
-    console.log(Kladblok)
-if (drank == "no"){
-    break
+    let drank = prompt("welke drankjes wilt u?, Bier, Fris, Wijn")
+    if(drank == 'Bier' || drank == 'Fris' || drank == 'Wijn'){
+        let aantal_drankjes = prompt('hoeveel ' +drank+ 'wilt u hebben?')
+        Kladblok[drank] = aantal_drankjes;
+        console.log(Kladblok)
+    
+    }
+    if (drank == "niks"){
+        break
+    
 }
 
 }
@@ -25,24 +28,6 @@ if (drank == "no"){
 // stap 1 is voeg een loop toe
 //stap 2 toevoegenen en updaten
 //laaste stap bon maken
-
-
-
-
-
-// rekening["Bier"] = 3;
-// rekening["wijn"] = 3;
-// console.log(rekening)
-
-// let vraag_01 = parseInt(prompt("Voer 1 in"));
-// let aantal_drankjes = parseInt(prompt("Hoeveel drankjes wilt u?,"));
-
-// let aantal_drankjes_02 = ''
-// for (let i =vraag_01; i <= aantal_drankjes; i++) {
-//          aantal_drankjes_02 = prompt("Welke drankjes wilt u?, Fris: 2,99 Wijn: 3,99 Bier: 3,99");
-//   }
-
-
 
 
 
