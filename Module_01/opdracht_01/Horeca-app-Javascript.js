bfcc4d74a0f17cc2ff649f04c46aa1a6e782f58c
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-03-17 13:05:01 +0100

    commit

--- Module_01/opdracht_01/Horeca-app-Javascript.js
+++ Module_01/opdracht_01/Horeca-app-Javascript.js
@@ -1,36 +1,50 @@
+
+let int_aantal_drank = 0
+
 const Drankenlijst = 
 {
-    'Bier': 3.99,
-    'Fris': 2.99,
-    'Wijn': 3.99
+    'Bier': 4,
+    'Fris': 3,
+    'Wijn': 4
 };
 
 let Kladblok = new Object();
 
+let drank = ''
+let aantal_drankjes = 0
 
 while (true){
-    let drank = prompt("welke drankjes wilt u?, Bier, Fris, Wijn")
-    if(drank == 'Bier' || drank == 'Fris' || drank == 'Wijn'){
-        let aantal_drankjes = prompt('hoeveel ' +drank+ 'wilt u hebben?')
-        Kladblok[drank] = aantal_drankjes;
-        console.log(Kladblok)
-    
+    drank = prompt("welke drankjes wilt u? , Bier, Fris, Wijn")
+    if(drank == "Bier" || drank == "Fris" || drank == "Wijn" ){
+        aantal_drankjes = prompt('hoeveel  '  +drank+  'wilt u hebben?')
+        int_aantal_drank = parseInt(aantal_drankjes)
+        if (drank in Kladblok){
+            Kladblok[drank] += int_aantal_drank
+       }
+       if(!(drank in Kladblok)){
+           Kladblok[drank] = int_aantal_drank;
+       }
     }
-    if (drank == "niks"){
-        break
-    
+    else if(drank != "stop" ){
+        alert("we hebben uw gewenste drankje niet")
+    }
+    console.log(Kladblok)
+    if (drank == "stop"){
+        break  
+    }
 }
 
-}
+let prijs_bier = Kladblok['Bier'] * Drankenlijst["Bier"]
+let prijs_fris = Kladblok['Fris']* Drankenlijst["Fris"]
+let prijs_wijn = Kladblok['Wijn'] * Drankenlijst["Wijn"]
+let totaal = prijs_bier + prijs_fris + prijs_wijn
 
 
-
-// stap 1 is voeg een loop toe
-//stap 2 toevoegenen en updaten
-//laaste stap bon maken
-
-
-
+console.log('-------------------\n' +
+'Bier: ' +prijs_bier+ 'euro, stuks' +Kladblok["bier"][int_aantal_drank] + '\n' +
+'Fris: ' +prijs_fris+ 'euro, stuks' +Kladblok["Fris"][int_aantal_drank] + '\n' +
+'Wijn: ' +prijs_wijn+ 'euro, stuks' +Kladblok["bier"][int_aantal_drank] + '\n' +
+'totaal: ' +totaal+ 'euro\n' )
 
 
 
