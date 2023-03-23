1c642e830bba72ff00ab99cf0cbaa99afeda703e
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-03-23 11:29:44 +0100

    push

--- Module_01/opdracht_01/Ruit.js
+++ Module_01/opdracht_01/Ruit.js
@@ -1,5 +1,19 @@
-let getal = parseInt(prompt("Typ een getal in: "))
+Antwoord_cijfer = []
+string = ''
 
-for (let x = 0; x < getal; x++){
-    
+let vraag_cijfer = prompt("voer een cijfer in.")
+for(let x = 1 ; x < vraag_cijfer +1 ; x++){
+    Antwoord_cijfer.push(x)
+    string += (Antwoord_cijfer.join('_')) + "\n";
 }
+
+for(let t = 1 ; t < vraag_cijfer +1 ; t++){
+    Antwoord_cijfer.pop(t)
+    string += (Antwoord_cijfer.join('_')) + "\n";
+}
+
+console.log(string)
+console.log(Antwoord_cijfer)
+
+document.getElementById("Antwoord_cijfer")
+document.getElementById("string")
