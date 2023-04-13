d85a64f207eda660c92ba19e32bec9bef5f00392
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-04-13 09:38:43 +0200

    commit

--- module_02/opdracht_01/button_1_Javascript.js
+++ module_02/opdracht_01/button_1_Javascript.js
@@ -1,4 +1,14 @@
-var button = document.getElementById("myButton");
+var button = document.getElementById("myButton_01");
+button.addEventListener("click", function() {
+  alert("De knop is geklikt!");
+});
+
+var button = document.getElementById("myButton_02");
+button.addEventListener("click", function() {
+  alert("De knop is geklikt!");
+});
+
+var button = document.getElementById("myButton_03");
 button.addEventListener("click", function() {
   alert("De knop is geklikt!");
 });
