45bd0ae272f8d97314eda8958eaecb8efa9b8d2f
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-04-13 11:56:00 +0200

    commit

--- module_02/opdracht_01/button_1_Javascript.js
+++ module_02/opdracht_01/button_1_Javascript.js
@@ -1,14 +1,27 @@
-var button = document.getElementById("myButton_01");
-button.addEventListener("click", function() {
-  alert("De knop is geklikt!");
-});
+let counter = 0;
 
-var button = document.getElementById("myButton_02");
-button.addEventListener("click", function() {
-  alert("De knop is geklikt!");
-});
+function Button_01() {
+    var afbeelding = document.getElementById("groote_cijfer");
+    afbeelding.src = "afbeeldingen/grote_1.png"
+    var afbeelding = document.getElementById("kleine_cijfer");
+    afbeelding.src = "afbeeldingen/kleine_1.png"
+    counter++;
+}
 
-var button = document.getElementById("myButton_03");
-button.addEventListener("click", function() {
-  alert("De knop is geklikt!");
-});
+function Button_02() {
+    var afbeelding = document.getElementById("groote_cijfer");
+    afbeelding.src = "afbeeldingen/grote_2.png"
+    var afbeelding = document.getElementById("kleine_cijfer");
+    afbeelding.src = "afbeeldingen/kleine_2.png"
+    counter++;
+}
+
+function Button_03() {
+    var afbeelding = document.getElementById("groote_cijfer");
+    afbeelding.src = "afbeeldingen/grote_3.png"
+    var afbeelding = document.getElementById("kleine_cijfer");
+    afbeelding.src = "afbeeldingen/kleine_3.png"
+    counter++;
+}
+
+console.log(counter)
