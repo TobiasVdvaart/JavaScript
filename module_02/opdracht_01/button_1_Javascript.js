4ec1f5684aa36850f42ca9dcb71e8222d6594ae5
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-06-21 12:03:09 +0200

    commit

--- module_02/opdracht_01/button_1_Javascript.js
+++ module_02/opdracht_01/button_1_Javascript.js
@@ -4,6 +4,8 @@ function counter(e){
     e.innerHTML = getal
 }
 
+var tem = ""
+
 function Handlebutton(e) {
     var afbeelding = document.getElementById("groote_cijfer");
     console.log(e.id)
@@ -11,5 +13,9 @@ function Handlebutton(e) {
     var afbeelding = document.getElementById("kleine_cijfer");
     afbeelding.src = "afbeeldingen/kleine_"+e.id+".png";
     counter(e)
+    document.getElementById(+e.id).disabled = true;
+    var tem = +e.id+
+    console.log(tem);
 }
 document.getElementById(1).style.backgroundColor == green
+    
